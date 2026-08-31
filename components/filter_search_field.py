0fff5e7689862a92340ca243cddcfb55d6701d4b
CarlosArthurM <arthurbispo1010@gmail.com>
2026-08-30 21:37:00 -0300

    fix: add 'All' option to platform filter

--- components/filter_search_field.py
+++ components/filter_search_field.py
@@ -2,5 +2,5 @@ import customtkinter as ctk
 
 class FilterCombox(ctk.CTkComboBox):
     def __init__(self, parent):
-        super().__init__(parent, values=["PC","PlayStation","Xbox","Nintendo"], corner_radius=10, state="readonly", width=150)
+        super().__init__(parent, values=["All","PC","PlayStation","Xbox","Nintendo"], corner_radius=10, state="readonly", width=150)
         self.set("Select a platform")
